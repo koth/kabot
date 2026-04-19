91f370fe3686948590c19f9148f0b462ec801c3c
koth <yovnchine@gmail.com>
2026-04-19 20:12:34 +0800

    fix task run

--- src/task/task_runtime.cpp
+++ src/task/task_runtime.cpp
@@ -34,14 +34,22 @@ std::string BuildWaitingKey(const std::string& channel_instance,
 std::string ExecuteCommand(const std::string& cmd) {
     std::array<char, 128> buffer{};
     std::string result;
+#if defined(_WIN32)
     FILE* pipe = _popen(cmd.c_str(), "r");
+#else
+    FILE* pipe = popen(cmd.c_str(), "r");
+#endif
     if (!pipe) {
         return "";
     }
     while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe) != nullptr) {
         result += buffer.data();
     }
+#if defined(_WIN32)
     _pclose(pipe);
+#else
+    pclose(pipe);
+#endif
     return result;
 }
 
